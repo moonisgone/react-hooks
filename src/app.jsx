876a0583c9d2c79b7bcf732690b43170974ee065
Moongon <moongon1004@gmail.com>
2021-05-26 14:14:48 +0900

    upload

---
+++ src/app.jsx
@@ -0,0 +1,101 @@
+import './app.css';
+import React, { useRef, useState  } from 'react';
+//import InputSample from './components/InputSample';
+import UserList from './components/UserList';
+import CreateUser from './components/CreateUser';
+
+function App() {
+
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: ''
+  });
+
+  const { username, email } = inputs;
+
+  const onChange = e => {
+    //console.log(e.target);
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com',
+      active: true
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com',
+      active: false
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com',
+      active: false
+    }
+  ]);
+  /*const i = Date.now();
+  const moongon = {
+      id : i,
+      username: 'Moongon',
+      email:'moongon1004@gamil.com',
+  }
+  users.push(moongon);
+*/
+  const nextId = useRef(4);
+
+  const onCreate = () => {
+    // 나중에 구현 할 배열에 항목 추가하는 로직
+    
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    };
+    setUsers([...users, user]);
+
+    setInputs({
+      username: '',
+      email: ''
+    });
+    nextId.current += 1;
+  };
+
+  const onRemove = id => {
+    console.log(id);
+    // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+    // = user.id 가 id 인 것을 제거함
+    setUsers(users.filter(user => user.id !== id));
+  };
+
+  const onToggle = id => {
+    setUsers(
+      users.map(user =>
+        user.id === id ? { ...user, active: !user.active } : user
+      )
+    );
+  };
+
+  return (
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+        
+      />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle}></UserList>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/UserList.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+function User({ user, onRemove, onToggle }) {
+    return (
+      <div>
+        <b style={{
+          cursor: 'pointer',
+          color: user.active ? 'green' : 'black'
+        }}
+        onClick={() => onToggle(user.id)}
+        >
+          {user.username}
+          </b>
+           <span>({user.email})</span>
+        <button onClick={() => onRemove(user.id)}>삭제</button>
+      </div>
+    );
+  }
+
+function UserList({users, onRemove, onToggle}) {
+    
+
+    return (
+        /*<div>
+      <div>
+        <b>{users[0].username}</b> <span>({users[0].email})</span>
+      </div>
+      <div>
+        <b>{users[1].username}</b> <span>({users[1].email})</span>
+      </div>
+      <div>
+        <b>{users[2].username}</b> <span>({users[1].email})</span>
+      </div>
+    </div>*/
+    /*
+    <>
+        <User user ={users[0]}></User>
+        <User user ={users[1]}></User>
+        <User user ={users[2]}></User>
+    </>
+    */
+    <>
+        {users.map(item => (
+            <User user={item} key={item.id} onRemove={onRemove} onToggle={onToggle}></User>
+
+        ))}
+    </>
+    );
+}
+
+export default UserList;
